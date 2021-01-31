0a5d2bf0131949e258aa6a90dfe6d818e58e6e8e
4ctiv <46281254+4ctiv@users.noreply.github.com>
2021-01-31 22:48:04 +0100

    Version 1.0.0 of Compress_with_ffmpeg.sh
    Created bash script to deliver functions of this project. 
    
    Note: To function the script needs ffmpeg installed and permissions to call ffmpeg.

---
+++ Compress_with_ffmpeg.sh
@@ -0,0 +1,37 @@
+NUM=0
+echo \$1 is $1
+if [ $1 = "-h" ]
+then
+    echo "Help for compress_with_ffmpeg.sh"
+    echo "-----------------------------------------"
+    echo " Usage:     sh compress_with_ffmpeg [Option] [ARGS...]"
+    echo " "
+    echo " Option     Meaning"
+    echo "   -h       Displays this message"
+    echo " "
+    echo " Example:   compress_with_ffmpeg \"nyan-cat_video.mp4\" \"other_random_video.mp4\" "
+    echo " "
+    echo "ffmpeg cmd: ffmpeg -i \"\$FILENAME\" -vcodec libx265 -crf 28 comressed_\"\$FILENAME\""
+    echo " "
+    echo " ARGS:      Arguments: Names of the to converting files."
+    echo "            The files have to be in the same folder, as the script and need to have the filename + file-extention correctly stated."
+    echo "            Watch out for spaces in filenames. You can either choose \" \ \" for spaces or put the filenames into doubble quotationmarks (see Example)."
+    echo "            Files can not be named \"-h\"  "
+else
+    for FILENAME in "$@"; do
+        
+        ffmpeg -i "$FILENAME" -vcodec libx265 -crf 28 comressed_"$FILENAME"
+        NUM=$(($NUM + 1))
+    done
+fi
+
+if [ $1 = "-h" ]
+then
+    echo
+elif [ $NUM = '0' ]; then
+    echo " "
+    echo "No Arguments, please use -h for help."
+else 
+    echo " "
+    echo "Done! Converted $NUM files."
+fi
