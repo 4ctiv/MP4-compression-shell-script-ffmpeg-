4b95faaad591af33d38eed837e188cadbe2896ac
Leif G <46281254+4ctiv@users.noreply.github.com>
2021-07-22 17:15:05 +0200

    Updated help (-h) message:

--- Compress_with_ffmpeg.sh
+++ Compress_with_ffmpeg.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 NUM=0
+REQ="ffmpeg"
+
+# check requirement installation
+requirement_check() {
+    echo "Checking $1 install ... "
+
+    if [ "$(command -v $1 | grep -o $1)" = "$REQ" ]; then
+        echo "Matched, $1 installed! "
+    else
+        echo "No Match, $1 missing! Exiting..."
+        exit
+    fi;
+}
+
+main() {
 # echo \$1 is "$1" # used for testing
 case "$1" in
     "-h")
@@ -15,6 +30,7 @@ case "$1" in
         echo 
         echo " Option     Meaning"
         echo "   -h       Displays this help message"
+        echo "   -a       Convert all *.mp4 files in the same folder as the script"
         echo
         echo " Example:   compress_with_ffmpeg \"nyan-cat_video.mp4\" \"other_random_video.mp4\" "
         echo
@@ -34,6 +50,7 @@ case "$1" in
             ffmpeg -i "$FILENAME" -map 0 -vcodec libx265 -crf 28 compressed_"$FILENAME"
             NUM=$($NUM + 1)
         done
+        ;;
     *)
         for FILENAME in "$@"; do
             echo 
@@ -53,3 +70,11 @@ case "$1" in
         fi
         ;;
 esac
+}
+
+# Run script
+requirement_check ffmpeg
+main "$@"
+
+#read -p "Press any key to continue... " -n 1 -s
+exit 0
